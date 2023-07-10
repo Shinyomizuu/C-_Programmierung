ecd8582d034293bf7e66035fbac5fce68d5db03f
Shinyomizuu <127198121+Shinyomizuu@users.noreply.github.com>
2023-07-10 13:22:31 +0200

    Projektdateien hinzufügen.

---
+++ ErstesProjekt/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace MyFirstProgramm
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            /*Frage 1.) Wie kann ich ein Projekt anlegen ? 
+             *          
+             *          Auf Datei drücken, neu, und dann Projekt oder beim Starten des Prgammes "neues Projekt anlegen" . 
+             *          
+             *Frage 2.) Was befindet sich alles im Projektmappen-Explorer?
+             *
+             *          Solutions: Die oberste Ebene der Projektmappe nennt sich Solutionm die gesmate Lösung in einem Arbeitsbereich darstellt
+             *          Solution können mehrere Projekte enthalten.
+             *          
+             *          Projects Unterhalb der Solution werden einzelne Projekte in der Lösung ausgeführt.
+             *          
+             *          Folders Innerhalb jedes Projekts können Ordner erstellt werden, die verschachtelt werden können um eine tiefe Strukturierung zu gewährleisten.
+             *          
+             *          Dann gibt es noch Files, References und Properties.
+             *          
+             *          
+             *Frage 3) Wie funktioniert der Debug Modus?
+             *
+             *         Bei der grauen Leiste kann man einen Breakpoint setzen. Dort beginnt es zu debuggen. Oben kann man dann weiter gehen mit den Schritten oder halt es wiederholen
+             *         oder stoppen
+             *         
+             *Frage 4.) Wie können einzelne Werte im Debug Modus überwacht werden.
+             *         
+             *          Es gibt unten eine Leiste mit , Auto, Lokal und Überwachen beim Überwachen1 kann man zB eine Variable eingeben die man angezheigt haben möchte. Lokal zeigt 
+             *          alle lokalten Variablen
+             *          
+             *          
+             * Frage 5) Wie kann ich Visual Studio mit Github - Account verbinden ? 
+             * 
+             *          Team Explorer auswählen
+             * 
+             */
+            Console.WriteLine("Hello World!");
+            int myVar = 1;
+            Console.WriteLine("Hello");
+        }
+    }
+}
